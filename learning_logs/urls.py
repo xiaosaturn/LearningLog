bf9641e8cbae6f8013271f50aa94cc7d71708edc
Xiao <807132689@qq.com>
2017-10-18 17:55:58 +0800

    完善

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	#主页
+	url(r'^$',views.index,name='index'),
+
+	#显示所有主题
+	url(r'^topics/$',views.topics,name='topics'),
+
+	#特定主题的详细页面
+	url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+]
