7b768ea6873264a37cee5761c0dd49c99ceffb09
Xiao <807132689@qq.com>
2017-10-18 16:47:05 +0800

    创建主题和条目模型

---
+++ learning_logs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Topic(models.Model):
+	#用户学习的主题
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+	def __str__(self):
+		#返回模型的字符串表示
+		return self.text
+
+#主题下的条目，一对多关系
+class Entry(models.Model):
+	topic = models.ForeignKey(Topic)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	#存储用于管理模型的额外信息
+	class Meta():
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		return self.text[:50] + "..."
+
+			
---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/beeda/Desktop/P/github_LearningLog/LearningLog/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
